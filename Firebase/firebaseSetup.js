7f330256b78b96463401216ebbb79ea08340ea45
JielingGong <serenagong01@outlook.com>
2024-08-03 21:26:40 -0700

    Set up firebased SDK.

---
+++ Firebase/firebaseSetup.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDFVuZqJ7C2bTpEyoO_KZwdX9RkL4kYnU",
+  authDomain: "huskyjobhunttracker.firebaseapp.com",
+  projectId: "huskyjobhunttracker",
+  storageBucket: "huskyjobhunttracker.appspot.com",
+  messagingSenderId: "147904942391",
+  appId: "1:147904942391:web:da534610482707d910cab2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
