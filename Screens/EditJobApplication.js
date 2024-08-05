1df72e0a921f60cb3cd30a7abc178e8280c9264b
cathyfu1215 <phoenixfucong@gmail.com>
2024-08-05 12:50:02 -0700

    added the edit screen

--- Screens/EditJobApplication.js
+++ Screens/EditJobApplication.js
@@ -1,10 +1,53 @@
 import React from 'react'
-import {View,Text} from 'react-native'
+import { View, Text, Pressable } from 'react-native'
+import AddAJobApplication from './AddAJobApplication.js';
+import styles from '../styleHelper.js';
+import PressableButton from '../Components/PressableButton.js';
+import { useEffect } from 'react';
+import { Feather } from '@expo/vector-icons';
+import { Alert } from 'react-native';
+import { deleteFromDB } from '../Firebase/fireStoreHelper.js';
 
 
-function EditJobApplication() {
-    <View><Text>EditJobApplication</Text></View>
+function EditJobApplication(props) {
+    function deleteHandler() {
+       
+          Alert.alert(
+            // Title
+            'Important',
+            // Message
+            'Are you sure you want to delete this entry?',
+            [
+              // Array of buttons
+              {text: 'NO', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+              {text: 'YES', onPress: () => {
+                //deleteFromDB(props.route.params.data.id, 'jobRecords');
+                console.log('Delete Pressed', props.route.params.data.id, 'to be deleted');
+                props.navigation.goBack();}},
+            ]
+          );
+          
+        }
+      
+      useEffect(() => {
+        props.navigation.setOptions({
+          headerRight: () => {
+            return (
+              <PressableButton pressedFunction={deleteHandler} >
+                <Feather name="trash-2" size={24} color="black" />
+              </PressableButton>
+            );
+          },
+        });
+      }, []);
+      
 
-}
+     <AddAJobApplication navigation={props.navigation} route={props.route} />
+     
+     
+     
+    }
+
+
 
 export default EditJobApplication
