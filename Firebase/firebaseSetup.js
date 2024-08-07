77bc6a1152d9c2b3b581153eaf41410a3d1dc95d
cathyfu1215 <phoenixfucong@gmail.com>
2024-08-07 12:48:19 -0700

    added skeletons of auth process

--- Firebase/firebaseSetup.js
+++ Firebase/firebaseSetup.js
@@ -2,8 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId } from "@env";
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeAuth , getReactNativePersistence} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,3 +18,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
+
+//export authentification
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
