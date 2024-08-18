083174486c3f6b073f27a069b874b26748c374ad
JielingGong <serenagong01@outlook.com>
2024-08-18 14:07:33 -0700

    Complete the logic for this 24 hour reminder.

--- Components/NotificationManager2.js
+++ Components/NotificationManager2.js
@@ -1,3 +1,53 @@
 /**
  * This notification manager is used to set a notification for a todo 24h later.
  */
+import React from 'react'
+import { View, Text, Pressable } from 'react-native'
+import * as Notifications from "expo-notifications";
+
+function NotificationManager2(props) {
+    const scheduleNotificationHandler = async () => {
+        try {
+          if (await verifyPermissions()) {
+            let identifier = await Notifications.scheduleNotificationAsync({
+              content: {
+                title: 'Todo Notification',
+                body: props.notificationContent,
+              },
+              trigger: {
+                seconds: 20,
+                // date: new Date(Date.now() + 30 * 60 * 1000), // Trigger 30 minutes from now
+              },
+            });
+            console.log('notification scheduled:', identifier);
+          } else {
+            console.log('permissions for notification are not granted');
+          }
+        } catch (err) {
+          console.log('error scheduling notification:', err);
+        }
+      };
+    
+      const verifyPermissions = async () => {
+        const { status } = await Notifications.getPermissionsAsync();
+        if (status !== 'granted') {
+          const { status } = await Notifications.requestPermissionsAsync();
+          if (status !== 'granted') {
+            alert('You need to enable the permission for sending notifications');
+            return false;
+          }
+        }
+        return true;
+      };
+
+      return (
+        <View>
+            <Pressable style={{backgroundColor:'lightblue',padding:10,margin:10}} onPress={scheduleNotificationHandler}>
+                <Text style={{color:'black',alignSelf:'center'}}>2h Reminder</Text>
+            </Pressable>
+        </View>
+      )
+
+}
+
+export default NotificationManager2;
