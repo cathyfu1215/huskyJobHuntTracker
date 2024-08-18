adcc19a9cd3d74458b1e2ecb682b5d9c5498cd2a
JielingGong <serenagong01@outlook.com>
2024-08-18 15:27:10 -0700

    Added a function for fetching home location.

--- Firebase/firebaseHelper.js
+++ Firebase/firebaseHelper.js
@@ -189,7 +189,23 @@ export const fetchJobApplicationLocation = async (uid, jobApplicationRecordId) =
       return null;
     }
   } catch (error) {
-    console.error("Error fetching document: ", error);
+    console.error("Error fetching company location: ", error);
+  }
+};
+
+// Function to get the user's home location from the database
+export const fetchHomeLocation = async (uid, jobApplicationRecordId) => {
+  try {
+    const docRef = doc(database, 'users', uid, 'jobApplicationRecords', jobApplicationRecordId);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      return docSnap.data().homeLocation;
+    } else {
+      console.warn("No such document!");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error fetching home location: ", error);
   }
 };
 
